357e0ce029bc16750e24a7934ad83f2242e9af55
Elias Ferguson <eferguson00@stuy.edu>
2018-12-05 11:15:08 -0500

    added random objects into the main function for better testing

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,17 @@
+import java.util.*;
 public class Sorts {
   private int[] data;
   public static void main(String[] args) {
-    int[] ary = new int[]{1, 4, 6, 2, 4, 7, 5, 8, 1000, 0};
+    int[] ary = new int[10];
+    Random randgen = new Random();
+    for (int i = 0; i < 10; i++) {
+      ary[i] = (randgen.nextInt() % 100);
+    }
     System.out.println(toString(ary));
-    selectionsort(ary);
+    selectionSort(ary);
     System.out.println(toString(ary));
   }
-  public static void selectionsort(int[] ary) {
+  public static void selectionSort(int[] ary) {
     for (int i = 0; i < ary.length - 1; i++) {
       int currSmall = i;
       for (int i2 = i + 1; i2 < ary.length; i2++) {
