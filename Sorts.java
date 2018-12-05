f2c38aadbd4c6bdaf7e17ca7f4566500f62fd818
Elias Ferguson <eferguson00@stuy.edu>
2018-12-04 21:07:10 -0500

    created prelim sort method and test

--- Sorts.java
+++ Sorts.java
@@ -1,8 +1,23 @@
 public class Sorts {
   public static void main(String[] args) {
-
+    int[] ary = new int[]{1, 4, 6, 2, 4, 7, 5, 8, 1000, 0};
+    System.out.println(ary);
+    selectionsort(ary);
+    System.out.println(ary);
   }
   public static void selectionsort(int[] ary) {
-    
+    int currSmall = ary[0];
+    int idxSwitch = 0;
+    for (int i = 0; i < ary.length; i++) {
+      for (int i2 = i; i2 < ary.length; i2++) {
+        if (currSmall > ary[i2]) {
+          currSmall = ary[i2];
+          idxSwitch = i2;
+        }
+      }
+      int switcher = ary[i];
+      ary[i] = currSmall;
+      ary[idxSwitch] = switcher;
+    }
   }
 }
